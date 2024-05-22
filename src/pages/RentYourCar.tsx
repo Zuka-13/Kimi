acce288aff12941ff4b63486a21c586752f558ce
Hamza <hsmovich@gmail.com>
2024-05-22 19:07:24 +0200

    Clearing out unneeded componenets and cleaning up code on Rent your car page

--- src/pages/RentYourCar.tsx
+++ src/pages/RentYourCar.tsx
@@ -1,42 +1,34 @@
-import { FunctionComponent, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import FrameComponent2 from "../components/FrameComponent2";
+import { FunctionComponent } from "react";
 import FinancialFuture from "../components/FinancialFuture";
 import DivSeoPagesE9c47y from "../components/DivSeoPagesE9c47y";
 import CallToActionRentOutCar from "../components/CallToActionRentOutCar";
 import HeaderDesktop from "../components/HeaderDesktop";
 
 const RentYourCar: FunctionComponent = () => {
-  const navigate = useNavigate();
-
-  const onHomeTextClick = useCallback(() => {
-    navigate("/");
-  }, [navigate]);
-
-  const onDestinationsTextClick = useCallback(() => {
-    navigate("/destinations");
-  }, [navigate]);
-
-  const onAboutTextClick = useCallback(() => {
-    navigate("/about");
-  }, [navigate]);
-
-  const onContactTextClick = useCallback(() => {
-    navigate("/contact");
-  }, [navigate]);
-
-  const onLinkSeoPages1uv25j8ButtonClick = useCallback(() => {
-    navigate("/destinations");
-  }, [navigate]);
-
-  const onLinkSeoPages1uv25j8Button1Click = useCallback(() => {
-    navigate("/rent-your-car");
-  }, [navigate]);
-
+  
   return (
     <div className="w-full relative bg-darkslategray-200 flex flex-col items-start justify-start pt-0 px-0 pb-[314.4px] box-border gap-[43px] mix-blend-normal leading-[normal] tracking-[normal] mq750:gap-[21px]">
       <HeaderDesktop />
-      <FrameComponent2 />
+      <section className="w-[1189px] flex flex-row items-start justify-center py-0 px-5 box-border max-w-full text-left text-smi-2 text-whitesmoke-200 font-inter">
+      <div className="flex flex-col items-start justify-start gap-[40px] max-w-full mq750:gap-[20px]">
+        <div className="flex flex-col items-start justify-start gap-[20px] max-w-full">
+          <div className="relative tracking-[0.5px] leading-[16px] uppercase font-black inline-block min-w-[115px]">
+            Find your drive
+          </div>
+          <div className="flex flex-col items-start justify-start max-w-full text-54xl-3 text-whitesmoke-100 font-noto-serif">
+            <div className="relative tracking-[-1px] leading-[90px] font-black mq450:text-25xl mq450:leading-[54px] mq1050:text-40xl mq1050:leading-[72px]">{`Start a car sharing `}</div>
+            <div className="relative tracking-[-1px] leading-[90px] font-black inline-block max-w-full mq450:text-25xl mq450:leading-[54px] mq1050:text-40xl mq1050:leading-[72px]">
+              business on Kimi
+            </div>
+          </div>
+        </div>
+        <div className="rounded-lg bg-kimi-yellow flex flex-row items-start justify-start py-0 pr-[17px] pl-[18px] mix-blend-normal whitespace-nowrap text-mid-6 text-black border-[1px] border-solid border-blueviolet">
+          <b className="relative leading-[49px] inline-block min-w-[98px]">
+            Get started
+          </b>
+        </div>
+      </div>
+    </section>
       <img
         className="ml-[-1px] mb-[35px] self-stretch relative max-w-full overflow-hidden max-h-full object-cover mix-blend-normal"
         alt=""
