c260f35d4b9bdb2ea4db77927603cf78c9b269de
Hamza <hsmovich@gmail.com>
2024-05-23 16:18:53 +0200

    Adding documentation and cleaning up the code on the About us page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -79,7 +79,7 @@ const About: FunctionComponent = () => {
                 </div>
                 <div className="self-stretch h-1 relative bg-orange-400 mix-blend-normal" />
               </div>
-              {/* Subeader paragraph and CSS styling for it */}
+              {/* Subheader paragraph and CSS styling for it */}
               <div className="relative leading-[24px] inline-block max-w-full">
                 Kimi is the world’s best car sharing marketplace, where you can book the perfect car for <br />
                 wherever you’re going from a vibrant community of trusted hosts across the US, UK, Canada,<br />
@@ -95,44 +95,50 @@ const About: FunctionComponent = () => {
               </div>
           </div>
         </div>
+        {/* Calling the SubheroSection component */}
         <SubheroSection />
       </section>
+
+      {/* CSS Styling of the section using TailwindCSS */}
       <section className="self-stretch flex flex-row items-start justify-center py-0 px-5 box-border max-w-full text-left text-xl-5 text-white font-inter">
+        {/* CSS Styling of the section elements eg. Padding, Flex positioning and font size using TailwindCSS */}
       <div className="w-[939px] flex flex-col items-start justify-start gap-[79.3px] max-w-full mq950:gap-[40px] mq700:gap-[20px]">
+        {/* CSS Styling of the text inside of this section */}
         <div className="self-stretch flex flex-row items-start justify-center py-0 px-5 text-22xl-6 font-noto-serif">
+          {/* CSS Styling of the header text of this section */}
           <div className="relative tracking-[-0.5px] leading-[56px] font-black mq450:text-6xl mq450:leading-[34px] mq925:text-14xl mq925:leading-[45px]">
             Want to dive deeper?
           </div>
         </div>
+        {/* CSS styling of the image on the left side */}
         <div className="w-[928px] flex flex-row items-start justify-center gap-[60px] max-w-full mq925:flex-wrap mq925:gap-[30px]">
           <div className="flex-1 flex flex-row items-start justify-start relative min-w-[328px] max-w-full">
+            {/* Inserting the image on the left side */}
             <img
               className="h-full w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] max-w-full overflow-hidden max-h-full object-cover mix-blend-normal"
               alt=""
               src="/image-eiig1vx0-12@2x.png"
             />
+            {/* Orange overlay on the image on the lift side */}
             <div className="h-[283px] flex-1 relative bg-orange-500 mix-blend-normal max-w-full z-[1]" />
-          </div>
+            </div>
+            {/* CSS styling of the text section on the right side */}
           <div className="w-[364px] flex flex-col items-start justify-start pt-[30px] px-0 pb-0 box-border min-w-[364px] max-w-full mq450:min-w-full mq925:flex-1">
             <div className="self-stretch flex flex-col items-start justify-start gap-[12.5px] max-w-full">
+              {/* Adding and CSS Styling of the header of the text on the right side */}
               <div className="relative tracking-[-0.2px] leading-[28px] font-black mq450:text-base mq450:leading-[22px]">
                 Join the team
               </div>
+              {/* Adding and CSS styling of the paragraph of the text on the right side */}
               <div className="self-stretch flex flex-col items-start justify-start pt-0 px-0 pb-[11.5px] box-border gap-[12px] max-w-full text-base-4">
                 <div className="self-stretch flex flex-col items-start justify-start max-w-full">
                   <div className="relative leading-[24px] inline-block max-w-full">
-                    Recognized as A Great Place to Work®, Kimi
+                    Recognized as A Great Place to Work®, Kimi <br />
+                    prides itself on creating a supportive, down-to- <br />
+                    earth, pioneering, and efficient work environment. <br />
+                    Review open positions and come join us!
                   </div>
-                  <div className="relative leading-[24px] inline-block max-w-full z-[1]">
-                    prides itself on creating a supportive, down-to-
-                  </div>
-                  <div className="relative leading-[24px] z-[2]">
-                    earth, pioneering, and efficient work environment.
-                  </div>
-                </div>
-                <div className="relative leading-[24px]">
-                  Review open positions and come join us!
-                </div>
+                  
               </div>
               {/* Calling the button component and providing the text, routing and color for it */}
               <Button 
@@ -183,7 +189,9 @@ const About: FunctionComponent = () => {
           </div>
         </div>
       </div>
+      </div>
     </section>
+      {/* Calling the footer component */}
       <Footer1 />
     </div>
   );
