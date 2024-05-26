1ee83eddaf422146bbc39ac7f998014afd1de803
Hamza <hsmovich@gmail.com>
2024-05-26 17:06:24 +0200

    Added register page and fixed login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,57 +6,36 @@ import FooterDesktop from "../components/FooterDesktop";
 const Login: FunctionComponent = () => {
   return (
     // Styling the background and the layout of the page using TailWindCSS
-    <div className="w-full relative bg-darkslategray-200 flex flex-col items-start justify-start pt-0 px-0 pb-[50.3px] box-border gap-[50px] mix-blend-normal leading-[normal] tracking-[normal] mq450:gap-[31px] mq750:gap-[62px]">
+    <div className="w-full relative bg-darkslategray-200 flex flex-col items-center justify-center pt-0 px-0 pb-[50.3px] box-border gap-[50px] mix-blend-normal leading-[normal] tracking-[normal] mq450:gap-[31px] mq750:gap-[62px]">
       {/* Calling the header module */}
       <HeaderDesktop />
-      <main className="w-[1401.4px] flex flex-row items-start justify-start pt-[120px] px-[73px] box-border max-w-full lg:pl-9 lg:pr-9 lg:box-border">
-        <section className="flex-1 flex flex-col items-end justify-start gap-[127.5px] max-w-full text-left text-54xl-7 text-black font-noto-serif lg:gap-[64px] mq450:gap-[16px] mq750:gap-[32px]">
+      <main className="w-[701.4px] flex flex-row items-center justify-center pt-[50px] px-[73px] box-border max-w-full lg:pl-9 lg:pr-9 lg:box-border">
+        <section className="flex-1 flex flex-col items-center justify-center gap-[127.5px] max-w-full text-left text-54xl-7 text-black font-noto-serif lg:gap-[64px] mq450:gap-[16px] mq750:gap-[32px]">
           
         <div className="container flex flex-col mx-auto bg-white rounded-lg pt-12 my-5">
       <div className="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
         <div className="flex items-center justify-center w-full lg:p-12">
           <div className="flex items-center xl:p-10">
             <form className="flex flex-col w-full h-full pb-6 text-center bg-white rounded-3xl">
-              <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">Sign In</h3>
-              <p className="mb-4 text-grey-700">Enter your email and password</p>
-              <a className="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300">
-                <img className="h-5 mr-2" src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png" alt="Google logo"/>
-                Sign in with Google
-              </a>
-              <div className="flex items-center mb-3">
-                <hr className="h-0 border-b border-solid border-grey-500 grow"/>
-                <p className="mx-4 text-grey-600">or</p>
-                <hr className="h-0 border-b border-solid border-grey-500 grow"/>
-              </div>
+              <h3 className="mb-3 text-14xl font-extrabold text-dark-grey-900">Login</h3>
+              
               <label htmlFor="email" className="mb-2 text-sm text-start text-grey-900">Email*</label>
               <input id="email" type="email" placeholder="mail@loopple.com" className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"/>
               <label htmlFor="password" className="mb-2 text-sm text-start text-grey-900">Password*</label>
               <input id="password" type="password" placeholder="Enter a password" className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"/>
               <div className="flex flex-row justify-between mb-8">
-                <label className="relative inline-flex items-center mr-3 cursor-pointer select-none">
-                  <input type="checkbox" checked={true} className="sr-only peer"/>
-                  <div className="w-5 h-5 bg-white border-2 rounded-sm border-grey-500 peer peer-checked:border-0 peer-checked:bg-purple-blue-500">
-                    <img src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/icons/check.png" alt="tick"/>
-                  </div>
-                  <span className="ml-3 text-sm font-normal text-grey-900">Keep me logged in</span>
-                </label>
+                
                 <a href="#" className="mr-4 text-sm font-medium text-purple-blue-500">Forget password?</a>
               </div>
               <button className="w-full px-6 py-5 mb-5 text-sm font-bold leading-none text-white transition duration-300 md:w-96 rounded-2xl hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 bg-purple-blue-500">Sign In</button>
-              <p className="text-sm leading-relaxed text-grey-900">Not registered yet? <a href="#" className="font-bold text-grey-700">Create an Account</a></p>
+              <p className="text-sm leading-relaxed text-grey-900">Not registered yet? <a href="./Register" className="font-bold text-grey-700">Create an Account</a></p>
             </form>
           </div>
         </div>
       </div>
-      <div className="flex flex-wrap -mx-3 my-5">
-        <div className="w-full max-w-full sm:w-3/4 mx-auto text-center">
-          <p className="text-sm text-slate-500 py-1">
-            Tailwind CSS Component from <a href="https://www.loopple.com/theme/motion-landing-library?ref=tailwindcomponents" className="text-slate-700 hover:text-slate-900" target="_blank" rel="noopener noreferrer">Motion Landing Library</a> by <a href="https://www.loopple.com" className="text-slate-700 hover:text-slate-900" target="_blank" rel="noopener noreferrer">Loopple Builder</a>.
-          </p>
-        </div>
-      </div>
     </div>
-    
+
+
         </section>
       </main>
       <FooterDesktop />
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -10,7 +10,115 @@ const Register: FunctionComponent = () => {
       <HeaderDesktop />
       <main className="w-[1401.4px] flex flex-row items-start justify-start pt-[120px] px-[73px] box-border max-w-full lg:pl-9 lg:pr-9 lg:box-border">
         <section className="flex-1 flex flex-col items-end justify-start gap-[127.5px] max-w-full text-left text-54xl-7 text-black font-noto-serif lg:gap-[64px] mq450:gap-[16px] mq750:gap-[32px]">
-          
+        <div className="gradient-form h-full bg-neutral-200 dark:bg-neutral-700">
+      <div className="container h-full p-10">
+        <div className="flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+          <div className="w-full">
+            <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+              <div className="g-0 lg:flex lg:flex-wrap">
+                {/* Left column container */}
+                <div className="px-4 md:px-0 lg:w-6/12">
+                  <div className="md:mx-6 md:p-12">
+                    {/* Logo */}
+                    <div className="text-center">
+                      <img
+                        className="mx-auto w-48"
+                        src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                        alt="logo"
+                      />
+                      <h4 className="mb-12 mt-1 pb-1 text-xl font-semibold">
+                        We are The Lotus Team
+                      </h4>
+                    </div>
+
+                    <form>
+                      <p className="mb-4">Please register an account</p>
+                      {/* Username input */}
+                      <div className="relative mb-4">
+                        <input
+                          type="text"
+                          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary dark:text-white dark:placeholder:text-neutral-300"
+                          id="exampleFormControlInput1"
+                          placeholder="Username"
+                        />
+                        <label
+                          htmlFor="exampleFormControlInput1"
+                          className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary dark:text-neutral-400"
+                        >
+                          Email address
+                        </label>
+                      </div>
+
+                      {/* Password input */}
+                      <div className="relative mb-4">
+                        <input
+                          type="password"
+                          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary dark:text-white dark:placeholder:text-neutral-300"
+                          id="exampleFormControlInput11"
+                          placeholder="Password"
+                        />
+                        <label
+                          htmlFor="exampleFormControlInput11"
+                          className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary dark:text-neutral-400"
+                        >
+                          Password
+                        </label>
+                      </div>
+
+                      {/* Submit button */}
+                      <div className="mb-12 pb-1 pt-1 text-center">
+                        <button
+                          className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-lg transition duration-150 ease-in-out focus:outline-none focus:ring-0 active:shadow-lg"
+                          type="button"
+                          style={{
+                            background: 'linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)',
+                          }}
+                        >
+                          Sign up
+                        </button>
+
+                        {/* Forgot password link */}
+                        <a href="#!">Terms and conditions</a>
+                      </div>
+
+                      {/* Register button */}
+                      <div className="flex items-center justify-between pb-6">
+                        <p className="mb-0">Have an account?</p>
+                        <button
+                          type="button"
+                          className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out focus:outline-none focus:ring-0 active:text-danger-700"
+                        >
+                          Login
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+
+                {/* Right column container with background and description */}
+                <div
+                  className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-e-lg lg:rounded-bl-none"
+                  style={{ background: 'linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)' }}
+                >
+                  <div className="px-4 py-6 text-white md:mx-6 md:p-12">
+                    <h4 className="mb-6 text-xl font-semibold">
+                      We are more than just a company
+                    </h4>
+                    <p className="text-sm">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing
+                      elit, sed do eiusmod tempor incididunt ut labore et
+                      dolore magna aliqua. Ut enim ad minim veniam, quis
+                      nostrud exercitation ullamco laboris nisi ut aliquip ex
+                      ea commodo consequat.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
         </section>
       </main>
       <FooterDesktop />
