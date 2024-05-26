9bb6021076f92005169f3b2ab11053cb48d74b34
Hamza <hsmovich@gmail.com>
2024-05-26 14:47:08 +0200

    Fixing footer

--- src/components/FooterDesktop.tsx
+++ src/components/FooterDesktop.tsx
@@ -2,16 +2,17 @@ import { FunctionComponent } from "react";
 
 const FooterDesktop: FunctionComponent = () => {
   return (
-    <div className="flex-1 bg-darkslategray-200 box-border flex flex-row items-start justify-center pt-[23px] pb-9 pr-5 pl-[29px] mix-blend-normal max-w-full text-left text-xs-8 text-white font-inter border-t-[1px] border-solid border-darkslategray-200 border-b-[1px]">
-      <div className="w-[940px] flex flex-col items-start justify-start gap-[16px] mix-blend-normal max-w-full">
-        <div className="flex flex-col items-start justify-start mix-blend-normal max-w-full">
-          <div className="relative leading-[16px] font-medium">{`* Any personal insurance you may have that covers damage to the host’s vehicle would kick in before your protection plan, except in limited situations for trips booked `}</div>
-          <div className="relative leading-[16px] font-medium inline-block max-w-full">{`in Maryland, but this protects your own wallet. Liability insurance is provided under a policy issued to Kimi by Travelers Excess and Surplus Lines Company. Terms, `}</div>
-          <div className="relative leading-[16px] font-medium inline-block max-w-full">
-            conditions, and exclusions apply. The policy does not provide
-            coverage for damage to a host’s vehicle.
-          </div>
+    <div className="flex-1 bg-darkslategray-200 box-border flex flex-row items-start justify-center pt-[23px] pb-9 pr-5 pl-[29px] mix-blend-normal max-w-full text-justify text-xs-8 text-white font-inter border-t-[1px] border-solid border-darkslategray-200 border-b-[1px]">
+      <div className="w-[940px] flex flex-col items-start justify-center gap-[16px] mix-blend-normal max-w-full">
+        <div className="flex flex-col items-start justify-center mix-blend-normal max-w-full">
+          <div className="relative leading-[16px] font-medium">
+            * Any personal insurance you may have that covers damage to the host’s vehicle would kick in before your protection plan, except in limited situations for trips booked<br />
+            in Maryland, but this protects your own wallet. Liability insurance is provided under a policy issued to Kimi by Travelers Excess and Surplus Lines Company. Terms,<br />
+            conditions, and exclusions apply. The policy does not provide coverage for damage to a host’s vehicle.
+            </div>
         </div>
+
+        
         <div className="self-stretch flex flex-col items-start justify-start mix-blend-normal max-w-full">
           <div className="relative leading-[16px] font-medium inline-block max-w-full">
             <span>{`For questions or information about the third party liability insurance for trips in the US, consumers in Maryland and the licensed states listed `}</span>
